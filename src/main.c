1efec01f4addb48d1b4e52b38d1e4c84e6522309
OLD-DATA <104645915+OLD-DATA@users.noreply.github.com>
2023-12-13 10:01:32 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    char *filename = argv[1];
-    FILE *fp = fopen(filename, "r");
+#define BUFFER_SIZE 1024
 
-    if (fp == NULL)
-    {
-        printf("Error: could not open file %s \n", filename);
-        return 1;
+
+int main( int argc, char** argv ){
+
+    const char *delimiter_characters = " ";
+    const char *filename = argv[1];
+    FILE *input_file = fopen( filename, "r" );
+    char buffer[ BUFFER_SIZE ];
+    char *last_token;
+
+    if( input_file == NULL ){
+
+        fprintf( stderr, "Unable to open file %s\n", filename );
+
+    }else{
+
+        // Read each line into the buffer
+        while( fgets(buffer, BUFFER_SIZE, input_file) != NULL ){
+
+            // Write the line to stdout
+            //fputs( buffer, stdout );
+
+            // Gets each token as a string and prints it
+            last_token = strtok( buffer, delimiter_characters );
+            while( last_token != NULL ){
+                printf( "%s\n", last_token );
+                last_token = strtok( NULL, delimiter_characters );
+            }
+
+        }
+
+        if( ferror(input_file) ){
+            perror( "The following error occurred" );
+        }
+
+        fclose( input_file );
+
     }
-
-    char ch;
-    while ((ch = fgetc(fp)) != EOF){
-        putchar(ch);
-    }
-    printf("\n");
-    // close the file
-    fclose(fp);
 
     return 0;
+
 }
